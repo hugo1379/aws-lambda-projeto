e00d13b4b3cc57b492ffd1df78e4140a86b70858
hugo1379 <hugo1379y@gmail.com>
2023-09-30 20:08:57 -0300

    :bug: fix: corrige nome de recurso

--- s3.tf
+++ s3.tf
@@ -11,7 +11,7 @@ resource "aws_s3_bucket_notification" "bucket_input_notification" {
   bucket = aws_s3_bucket.bucket_input.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.lambda_function.arn
+    lambda_function_arn = aws_lambda_function.lambda_function_projeto.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "pessoa/"
     filter_suffix       = ".png"
