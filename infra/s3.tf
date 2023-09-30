46fae0a6df5da0939124caa2d80fb708324991ba
hugo1379 <hugo1379y@gmail.com>
2023-09-30 18:29:18 -0300

    :bricks: ci: provisiona bucket + trigger

---
+++ infra/s3.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "bucket_input" {
+  bucket = "bucket_input"
+
+}
+
+resource "aws_s3_bucket" "bucket_output" {
+  bucket = "bucket_output"
+}
+
+resource "aws_s3_bucket_notification" "bucket_input_notification" {
+  bucket = aws_s3_bucket.bucket_input.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.lambda_function.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "pessoa/"
+    filter_suffix       = ".png"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
